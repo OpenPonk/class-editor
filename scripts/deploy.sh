8495378d61f9980fab3a2e2194fbe77909aac2f3
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2020-01-21 22:46:59 +0100

    enabled deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,41 +1,128 @@
 #!/bin/bash
 
-set -euo pipefail
-IFS=$'\n\t'
+set -euxo pipefail
 
-readonly BRANCH_NAME="${TRAVIS_BRANCH:-$CI_COMMIT_REF_NAME}"
-readonly _TAG_NAME="${CI_COMMIT_TAG:-}"
-readonly TAG_NAME="${TRAVIS_TAG:-$_TAG_NAME}"
-readonly TAG_VERSION="${TAG_NAME#v}"
-readonly CI_BUILD_ID="${TRAVIS_BUILD_NUMBER:-$CI_JOB_ID}"
-readonly BUILD_ID="${TAG_VERSION:-"${BRANCH_NAME}-${CI_BUILD_ID}"}"
+prepare_directory() {
 
-# wherever you'll be ssh-ing into user@machine
-readonly TARGET_MACHINE="openponk@ccmi.fit.cvut.cz"
-# target dir on the target machine
-readonly UPLOAD_DIR="~/uploads/uml"
+	local platform=$1
+	local package_dir=$2
 
-# customize the name of the Pharo image you will be deploying
-readonly PROJECT_NAME="openponk"
-readonly ARTIFACT_ZIP="${PROJECT_NAME}-image-${BUILD_ID}.zip"
+	mkdir -p "$package_dir"
 
-# zip the image, and upload it to the server
-deploy-scp() {
-	local directory=$1
-	local zip=$2
-	zip -qr "$zip" "$directory"
-	scp -rp "$zip" "$TARGET_MACHINE:$UPLOAD_DIR"
-	# I have a server-side post-processing script that bundles VMs into the build
-	ssh "$TARGET_MACHINE" "~/scripts/process-uml-build.sh ${BUILD_ID}"
+	cp $SMALLTALK_CI_IMAGE $package_dir/openponk-$PROJECT_NAME.image
+	cp $SMALLTALK_CI_CHANGES $package_dir/openponk-$PROJECT_NAME.changes
+	cp $SMALLTALK_CI_BUILD_BASE/*.sources $package_dir
+	echo "$PHARO_VERSION_SIMPLE" > $package_dir/pharo.version
+}
+
+download_vm() {
+
+	local platform=$1
+	local vm_dir=$2
+	local zip="$vm_dir/vm.zip"
+
+	mkdir -p "$vm_dir"
+
+	curl --location --compressed --output "$zip" "http://files.pharo.org/get-files/$PHARO_VERSION_SIMPLE/pharo${PHARO_BITS//32}-${platform}-stable.zip"
+	unzip "$zip" -d "$vm_dir"
+	rm "$zip"
+}
+
+upload() {	
+
+	local platform=$1
+	local package_dir_name=$2
+	local working_dir="$package_dir_name-$PHARO_FULL_VERSION-build$TRAVIS_BUILD_NUMBER"
+	local package_dir="$working_dir/$package_dir_name"
+	local zip="$working_dir.zip"
+
+	cd $working_dir
+	zip -qr "$zip" "${package_dir_name}"
+
+	set +x
+		curl -k -v -u "${DEPLOY_KEY}" --upload-file $zip https://nexus.openponk.ccmi.fit.cvut.cz/repository/$repository/byplatform/"${PHARO_FULL_VERSION}"/"${platform}"/"${PROJECT_NAME}"/${zip}
+		curl -k -v -u "${DEPLOY_KEY}" --upload-file $zip https://nexus.openponk.ccmi.fit.cvut.cz/repository/$repository/byversion/"${PROJECT_NAME}"/"$BUILD_NAME"/${zip}
+	set -x
+}
+
+deploy_linux() {
+
+	local platform="linux"
+
+	local package_dir_name="openponk-$PROJECT_NAME-$platform"
+	local working_dir="$package_dir_name-$PHARO_FULL_VERSION-build$TRAVIS_BUILD_NUMBER"
+	local package_dir="$working_dir/$package_dir_name"
+	local vm_dir="$package_dir"
+
+	prepare_directory $platform $package_dir
+	download_vm "$platform-threaded" $vm_dir
+
+	rm $vm_dir/pharo
+	cat << EOF > $vm_dir/openponk-$PROJECT_NAME
+#!/bin/bash
+./bin/pharo openponk-$PROJECT_NAME.image
+EOF
+	chmod a+rx $vm_dir/openponk-$PROJECT_NAME
+
+	upload $platform "$package_dir_name"
+
+}
+
+deploy_windows() {
+
+	local platform="win"
+
+	local package_dir_name="openponk-$PROJECT_NAME-$platform"
+	local working_dir="$package_dir_name-$PHARO_FULL_VERSION-build$TRAVIS_BUILD_NUMBER"
+	local package_dir="$working_dir/$package_dir_name"
+	local vm_dir="$package_dir"
+
+	prepare_directory $platform $package_dir
+	download_vm $platform $vm_dir
+	
+	mv $vm_dir/*haro.exe $vm_dir/openponk-$PROJECT_NAME.exe
+	rm $vm_dir/*haroConsole.exe
+	rm $vm_dir/*Zone.Identifier || true
+
+	upload $platform "$package_dir_name"
+
+}
+
+deploy_image() {
+	
+	local platform="image"
+
+	local package_dir_name="openponk-$PROJECT_NAME-$platform"
+	local working_dir="$package_dir_name-$PHARO_FULL_VERSION-build$TRAVIS_BUILD_NUMBER"
+	local package_dir="$working_dir/$package_dir_name"
+
+	prepare_directory $platform $package_dir
+
+	upload $platform "$package_dir_name"
+
 }
 
 main() {
-	local directory="${PROJECT_NAME}-${BUILD_ID}"
-	mv $ARTIFACT_DIR $directory
-	deploy-scp $directory $ARTIFACT_ZIP
-	echo "Build ${BUILD_ID} deployed."
+
+	export PHARO_VERSION_SIMPLE="${PHARO_VERSION//.}"
+	export PHARO_FULL_VERSION="pharo$PHARO_VERSION-${PHARO_BITS}b"
+
+	deploy_image
+	deploy_linux
+	deploy_windows
 }
 
-if [ "$BRANCH_NAME" = "master" ] || [ -n "$TAG_NAME" ]; then
+export TRAVIS_BUILD_NUMBER=`printf "%04d\n" $TRAVIS_BUILD_NUMBER`
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+	export BUILD_NAME="build$TRAVIS_BUILD_NUMBER"
+	export repository="builds"
 	main
 fi
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+	export BUILD_NAME="$TRAVIS_TAG"
+	export repository="releases"
+	main
+fi
+
